bb911a1f54b34f1bbc6d9ee9b20ca69f167ac327
nicki baldwin <nickibaldwin@gmail.com>
2021-01-15 17:41:17 -0800

    final update

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var count =0
 var userName =prompt('Hello! What is your name? ');
 document.write('Welcome, ' + userName)
-console.log('what is the user name?')
 
 var userDay =prompt('Hello! Are you having a good day today? y/n') ;
 if(userDay.toLowerCase() == 'y') {
@@ -10,7 +9,6 @@ if(userDay.toLowerCase() == 'y') {
 } else {
   document.write('I am sorry to hear that!')
 }
-console.log('The user is having a good day' + userDay )
 
 var userSun =prompt('Is it sunny where you are?');
 if(userSun.toLowerCase() == 'y'){
@@ -18,7 +16,6 @@ if(userSun.toLowerCase() == 'y'){
 } else {
   document.write('It is not sunny here, either.');
 }
-console.log('Is it sunny?' + userSun )
 
 var userWarm =prompt('Do you like warm weather? ');
 if(userSun.toLowerCase() == 'y'){
@@ -26,7 +23,6 @@ if(userSun.toLowerCase() == 'y'){
 } else {
   document.write('Ah, more a cool weather person. n-ice');
 }
-console.log('Does user like warm weather?')
 
 var userSnow =prompt('Or, do you prefer the cold?');
 if(userSun.toLowerCase() == 'y'){
@@ -34,7 +30,6 @@ if(userSun.toLowerCase() == 'y'){
 } else {
   document.write('SAME');
 }
-console.log('Does user like cold?')
 
 var userRain =prompt('Do you like the rain, ' + userName + '?');
 if(userRain.toLowerCase() == 'y'){
@@ -42,28 +37,23 @@ if(userRain.toLowerCase() == 'y'){
 } else {
   document.write('same, ' + userName + '! dreaming of a sunny beach rn');
 }
-console.log('Does user like rain?')
 
 for (var i=0; i < 4; i++){
   var userNumber = prompt(userName + ' please guess a number between 1-10.');
   userNumber = parseInt(userNumber)
 if(i===3 && userNumber !==5){
   alert('Good guessing, ' + userName + ', but the correct answer was 5!')
-  console.log('User did not guess correctly in alloted amount of times')
 }
   else if(userNumber === 5){
     alert('You got it, ' + userName + '!!!! Great guessing!');
-    console.log('User guessed correctly')
     count++ 
     break;
   }
   else if(userNumber < 5){
     alert('Too low! Please guess again!')
-    console.log('User guessed too low')
   }
   else if(userNumber > 5){
     alert('Too high! Please guess again!')
-    console.log('User guessed too high')
   }
 }
 
@@ -76,20 +66,17 @@ for (var i=0; i <6; i++){
     if(userTravel.toLowerCase() === travelArray[j]){
       alert('Great Job, ' + userName + '!!')
       flag = false
-      console.log('User guessed correctly')
       break;
     }
   }
   if(flag === false){
-    console.log('User guessed incorrectly')
     break;
   }
   if(i===5 && flag===true){
     alert('The correct answers were Cuba and Kauai, good luck next time!')
-    console.log('Display correct answers for user')
   } else{alert('Guess again!')}
-    console.log('alert the user to guess again')
 }
+
 alert('Congrats! You got ' + count + ' correct!')
-console.log('Inform user of count of the correct answers')
+
 
